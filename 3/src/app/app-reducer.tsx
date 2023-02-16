0658c4b7aec670d9eef34e4c12531edee38c2efb
Nadyushka <12nadya11@gmail.com>
2023-02-16 20:09:54 +0300

    lessom 15 add Error show

---
+++ 3/src/app/app-reducer.tsx
@@ -0,0 +1,31 @@
+//app-reducer.tsx
+
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'loading' as RequestStatusType,
+    error: null as null | string
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR': {
+            return {...state, error: action.error}
+        }
+        default:
+            return state
+    }
+}
+
+type ActionsType = SetLoadingStatusType | SetErrorType
+
+export const setLoadingStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setErrorAC = (error: null | string) => ({type: 'APP/SET-ERROR', error} as const)
+
+export type SetLoadingStatusType = ReturnType<typeof setLoadingStatusAC>;
+export type SetErrorType = ReturnType<typeof setErrorAC>;
